ff4a1f05ad98a729718fabf29c775fa8dc0fb35c
danielginz <42783698+danielginz@users.noreply.github.com>
2023-02-19 17:45:32 +0200

    Add files via upload

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -44,7 +44,20 @@ function BurgerIngredients(props) {
 }
 
 BurgerIngredients.propTypes = {
-    items: PropTypes.arrayOf(PropTypes.shape({type: PropTypes.string.isRequired}).isRequired).isRequired
+    items: PropTypes.arrayOf(PropTypes.shape({
+        __v: PropTypes.number.isRequired,
+        _id: PropTypes.string.isRequired,
+        calories: PropTypes.number.isRequired,
+        carbohydrates: PropTypes.number.isRequired,
+        fat: PropTypes.number.isRequired,
+        image: PropTypes.string.isRequired,
+        image_large: PropTypes.string.isRequired,
+        image_mobile: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        price: PropTypes.number.isRequired,
+        proteins: PropTypes.number.isRequired,
+        type: PropTypes.string.isRequired
+    }).isRequired).isRequired
 };
 
 export default BurgerIngredients;
