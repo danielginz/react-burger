77a0385ae1e9f27f2e071c404dd89675c744dd63
danielginz <42783698+danielginz@users.noreply.github.com>
2023-02-20 14:46:20 +0200

    Add files via upload

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import PropTypes from 'prop-types';
 import burgerConstructorStyles from './burger-constructor.module.css';
 import { ConstructorElement, DragIcon, CurrencyIcon, Button } from '@ya.praktikum/react-developer-burger-ui-components';
 
 function BurgerConstructor(props) {
+    const bun = props.items.find(item => item.type === 'bun');
+    const ingredients = props.items.filter(item => (item.type !== 'bun')).slice(4, 12);
 
+    const calcPrice = (bun) => {
+        if(bun != undefined) {
+            const price = bun.price * 2 + ingredients.reduce((acc, p) => acc + p.price, 0);
+            console.log("AAA, calcPrice, price: "+price);
+        }
+    };
+    const calculatePrice = useMemo(() => calcPrice(), []);
+
+
+    //console.log("AAA, burger-constructor, props.items: "+JSON.stringify(props.items));
+    //console.log("AAA, burger-constructor, ingridients: "+JSON.stringify(ingredients));//it's working
     return(
         <>
             <ul className={burgerConstructorStyles.burger_constructor_list + ' ml-4 mt-25 mb-10 pr-4'}>
-                <li className='pl-8' key="top_bun">
+                <li className='pl-8'>
                     <ConstructorElement
                         type='top'
                         isLocked={true}
-                        text={props.bunItem.name + ' (верх)'}
-                        thumbnail={props.bunItem.image}
-                        price={props.bunItem.price}
+                        text={bun.name + ' (верх)'}
+                        thumbnail={bun.image}
+                        price={bun.price}
                     />
                 </li>
                 <li>
-                    {(props.middleItems.length > 0 ?
+                    {(ingredients.length > 0 ?
                             <ul className={burgerConstructorStyles.burger_constructor_draggable_list + ' pr-2'} key="middle_items">
-                                {props.middleItems.map((item, index) => (
+                                {ingredients.map((item, index) => (
                                     <li className={burgerConstructorStyles.burger_constructor_draggable_list_item}
                                         key={item._id}>
                                     <span className={burgerConstructorStyles.burger_constructor_drag_icon}>
@@ -44,17 +57,17 @@ function BurgerConstructor(props) {
                     <ConstructorElement
                         isLocked={true}
                         type='bottom'
-                        text={props.bunItem.name + ' (низ)'}
-                        thumbnail={props.bunItem.image}
-                        price={props.bunItem.price}
+                        text={bun.name + ' (низ)'}
+                        thumbnail={bun.image}
+                        price={bun.price}
                     />
                 </li>
             </ul>
             <div className={burgerConstructorStyles.burger_constructor_order + ' mr-4 mb-10'}>
                 <p className="text text_type_digits-medium">
                     {
-                        props.bunItem.price * 2 +
-                        props.middleItems.reduce((acc, p) => acc + p.price, 0)
+                        calculatePrice
+                        //bun.price * 2 + ingredients.reduce((acc, p) => acc + p.price, 0)
                     }
                 </p>
                 <span className='ml-2 mr-10'>
@@ -69,13 +82,13 @@ function BurgerConstructor(props) {
 }
 
 BurgerConstructor.propTypes = {
-    bunItem: PropTypes.shape({
+    bun: PropTypes.shape({
         name: PropTypes.string.isRequired,
         price: PropTypes.number.isRequired,
         image: PropTypes.string.isRequired
     }),
 
-    middleItems: PropTypes.arrayOf(PropTypes.shape({
+    ingredients: PropTypes.arrayOf(PropTypes.shape({
         name: PropTypes.string.isRequired,
         price: PropTypes.number.isRequired,
         image: PropTypes.string.isRequired,
