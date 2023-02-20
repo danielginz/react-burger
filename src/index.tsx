eb01aa9ed75c686d6fc2fd38e1abb2f7828c3be0
danielginz <42783698+danielginz@users.noreply.github.com>
2023-02-20 14:44:41 +0200

    Add files via upload

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
 
 import './index.module.css';
 import App from './components/app/app.jsx';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
+root.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>,
-    document.getElementById('root')
 );
