1ad85966cbbbbf4cc68972e45d09dfe91a740aa6
danielginz <42783698+danielginz@users.noreply.github.com>
2023-03-01 15:10:12 +0200

    Add files via upload

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,96 +1,158 @@
-import React, {useMemo} from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useDrop } from 'react-dnd';
+
 import burgerConstructorStyles from './burger-constructor.module.css';
-import { ConstructorElement, DragIcon, CurrencyIcon, Button } from '@ya.praktikum/react-developer-burger-ui-components';
-import {ingredientType} from '../../utils/prop-types';
+import { ConstructorElement, CurrencyIcon, Button } from '@ya.praktikum/react-developer-burger-ui-components';
+import DraggableConstructorElement from '../draggable-constructor-element/draggable-constructor-element';
+import { placeOrder } from '../../services/slices/order';
 
-function BurgerConstructor(props) {
-    const bun = props.items.find(item => item.type === 'bun');
-    const ingredients = props.items.filter(item => (item.type !== 'bun')).slice(4, 12);
+import { burgerConstructorSlice } from '../../services/slices/burger-constructor';
+import { itemsSlice } from '../../services/slices/items';
 
-    const totalPrice = useMemo(() => {
-        return bun.price * 2 + ingredients.reduce((acc, p) => acc + p.price, 0);
-    }, [ingredients, bun]);
+function BurgerConstructor() {
+    const dispatch = useDispatch();
+    const { increaseQuantityValue, decreaseQuantityValue } = itemsSlice.actions;
+    const { setBunItem, calcTotalPrice } = burgerConstructorSlice.actions
+    const { bunItem, middleItems, totalPrice } = useSelector(state => state.burgerConstructor);
+
+    const onOrderButtonClick = () => {
+        const items = [bunItem._id];
+        middleItems.map(item => items.push(item._id));
+        dispatch(placeOrder(items));
+    };
+
+    useEffect(() => {
+        dispatch(calcTotalPrice());
+    }, [dispatch, bunItem, middleItems, calcTotalPrice]);
+
+
+    const handleBunItemDrop = (newBunItem) => {
+        dispatch(setBunItem(newBunItem));
+        dispatch(decreaseQuantityValue(bunItem._id));
+        dispatch(decreaseQuantityValue(bunItem._id));
+        dispatch(increaseQuantityValue(newBunItem._id));
+        dispatch(increaseQuantityValue(newBunItem._id));
+    };
+
+    const [, dropTopBunTarget] = useDrop({
+        accept: 'bun',
+        drop(newBunItem) {
+            handleBunItemDrop(newBunItem);
+        }
+    });
+
+    const [, dropBottomBunTarget] = useDrop({
+        accept: 'bun',
+        drop(newBunItem) {
+            handleBunItemDrop(newBunItem);
+        }
+    });
+
+    const [, dropMiddleItemTarget] = useDrop({
+        accept: ['sauce', 'main']
+    });
+
+    const generateItemHash = () => (
+        Math.floor(Math.random() * 10000)
+    );
 
     return(
         <>
             <ul className={burgerConstructorStyles.burger_constructor_list + ' ml-4 mt-25 mb-10 pr-4'}>
-                <li className='pl-8'>
-                    <ConstructorElement
-                        type='top'
-                        isLocked={true}
-                        text={bun.name + ' (верх)'}
-                        thumbnail={bun.image}
-                        price={bun.price}
-                    />
+                <li className='pl-8' ref={dropTopBunTarget}>
+                    {!!bunItem.name ? (
+                        <ConstructorElement
+                            type='top'
+                            isLocked={true}
+                            text={bunItem.name + ' (верх)'}
+                            thumbnail={bunItem.image}
+                            price={bunItem.price}
+                        />
+                    ) : (
+                        <div
+                            className={
+                                burgerConstructorStyles.emptyBun +
+                                ' constructor-element constructor-element_pos_top'
+                            }
+                        >
+                            &nbsp;
+                        </div>
+                    )}
                 </li>
-                <li>
-                    {(ingredients.length > 0 ?
-                            <ul className={burgerConstructorStyles.burger_constructor_draggable_list + ' pr-2'} key="middle_items">
-                                {ingredients.map((item, index) => (
-                                    <li className={burgerConstructorStyles.burger_constructor_draggable_list_item}
-                                        key={item._id}>
-                                    <span className={burgerConstructorStyles.burger_constructor_drag_icon}>
-                                        <DragIcon type='primary' />
-                                    </span>
-                                        <ConstructorElement
-                                            text={item.name}
-                                            thumbnail={item.image}
-                                            price={item.price}
-                                        />
-                                    </li>
+                <li ref={dropMiddleItemTarget}>
+                    {(middleItems.length > 0 ?
+                            <ul
+                                className={burgerConstructorStyles.burger_constructor_draggable_list + ' pr-2'}
+                                key='middle_items'
+                            >
+                                {middleItems.map((item, index) => (
+                                    <DraggableConstructorElement
+                                        item={item}
+                                        index={index}
+                                        // key should have random generated hash or timestamp added to '_id'
+                                        key={item._id+generateItemHash()}
+                                    />
                                 ))}
                             </ul>
                             :
-                            <h3 className={burgerConstructorStyles.warningText + ' text text_type_main-default text_color_inactive pt-6 pb-6'}>
-                                Добавьте ингредиенты
+                            <h3
+                                className={burgerConstructorStyles.warningText +
+                                    ' text text_type_main-default text_color_inactive pt-6 pb-6'}
+
+                            >
+                                {totalPrice === 0 ? (
+                                    'Добавьте булку и ингредиенты'
+                                ) : (
+                                    'Добавьте ингредиенты'
+                                )}
                             </h3>
                     )}
                 </li>
-                <li className='pl-8' key="bottom_bun">
-                    <ConstructorElement
-                        isLocked={true}
-                        type='bottom'
-                        text={bun.name + ' (низ)'}
-                        thumbnail={bun.image}
-                        price={bun.price}
-                    />
+                <li className='pl-8' ref={dropBottomBunTarget}>
+                    {!!bunItem.name ? (
+                        <ConstructorElement
+                            isLocked={true}
+                            type='bottom'
+                            text={bunItem.name + ' (низ)'}
+                            thumbnail={bunItem.image}
+                            price={bunItem.price}
+                        />
+                    ) : (
+                        <div
+                            className={
+                                burgerConstructorStyles.emptyBun +
+                                ' constructor-element constructor-element_pos_bottom'
+                            }
+                        >
+                            &nbsp;
+                        </div>
+                    )}
                 </li>
             </ul>
-            <div className={burgerConstructorStyles.burger_constructor_order + ' mr-4 mb-10'}>
-                <p className="text text_type_digits-medium">
-                    {
-                        totalPrice
-                    }
+            <div className={
+                `${burgerConstructorStyles.burger_constructor_order}
+                    mr-4 mb-10
+                    ${!bunItem.name ? burgerConstructorStyles.disabled : null}`
+            }
+            >
+                <p className='text text_type_digits-medium'>
+                    {totalPrice}
                 </p>
                 <span className='ml-2 mr-10'>
-                    <CurrencyIcon type="primary" />
+                    <CurrencyIcon type='primary' />
                 </span>
-                <Button htmlType="button" type="primary" size="medium" onClick={props.onOrderButtonClick}>
+                <Button
+                    htmlType="button"
+                    type="primary"
+                    size="medium"
+                    onClick={bunItem.name ? onOrderButtonClick : null}
+                >
                     Оформить заказ
                 </Button>
             </div>
         </>
     );
 }
-
-BurgerConstructor.propTypes = {
-    bun: PropTypes.shape({
-        name: PropTypes.string.isRequired,
-        price: PropTypes.number.isRequired,
-        image: PropTypes.string.isRequired
-    }),
-
-    ingredients: PropTypes.arrayOf(PropTypes.shape({
-        name: PropTypes.string.isRequired,
-        price: PropTypes.number.isRequired,
-        image: PropTypes.string.isRequired,
-        _id: PropTypes.string.isRequired
-    })),
-
-    items: PropTypes.arrayOf(ingredientType.isRequired).isRequired,
-    onOrderButtonClick: PropTypes.func.isRequired,
-
-};
 
 export default BurgerConstructor;
