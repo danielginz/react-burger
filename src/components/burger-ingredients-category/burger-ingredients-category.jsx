964aa7cb4e6ecff31d2981ff7a95e94af7bcf551
danielginz <42783698+danielginz@users.noreply.github.com>
2023-03-01 15:12:28 +0200

    Add files via upload

--- src/components/burger-ingredients-category/burger-ingredients-category.jsx
+++ src/components/burger-ingredients-category/burger-ingredients-category.jsx
@@ -1,31 +1,33 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 import burgerIngredientsCategoryStyles from './burger-ingredients-category.module.css';
 import BurgerIngredientsCard from '../burger-ingredients-card/burger-ingredients-card';
-import {ingredientType} from '../../utils/prop-types';
+import {ingredientSimplifiedType} from '../../utils/prop-types';
 
-function BurgerIngredientsCategory(props) {
-    return(
-        <section>
-            <h2 className="text text_type_main-medium mt-10 mb-6">
-                {props.heading}
-            </h2>
-            {(props.items.length > 0 ?
-                <ul className={burgerIngredientsCategoryStyles.burger_ingredients_list + ' ml-4 mt-6 mr-2 mb-10'}>
-                    {props.items.map((item) =>
-                        <BurgerIngredientsCard
-                            name={item.name} price={item.price} image={item.image}
-                            value={item.__v} id={item._id} key={item._id} onIngredientClick={props.onIngredientClick}/>)}
-                </ul>
-                : <h3 className='text text_type_main-default text_color_inactive pb-6'>
-                    Категория пуста
-                </h3>)}
-        </section>
-    );
-}
+const BurgerIngredientsCategory = forwardRef((props, ref) => (
+    <section id={props.categoryId} ref={ref}>
+        <h2 className="text text_type_main-medium pt-10 mb-6">
+            {props.heading}
+        </h2>
+        {/* when items aren't received, show 'Empty category' message */}
+        {(props.items.length > 0 ?
+            <ul className={burgerIngredientsCategoryStyles.burger_ingredients_list + ' ml-4 mt-6 mr-2 mb-10'}>
+                {props.items.map((item) => (
+                    <BurgerIngredientsCard
+                        key={item._id}
+                        item={item}
+                    />
+                ))}
+            </ul>
+            : <h3 className='text text_type_main-default text_color_inactive pb-6'>
+                Категория пуста
+            </h3>)}
+    </section>
+));
 
 BurgerIngredientsCategory.propTypes = {
-    items: PropTypes.arrayOf(ingredientType.isRequired).isRequired
+    heading: PropTypes.string.isRequired,
+    items: PropTypes.arrayOf(ingredientSimplifiedType.isRequired).isRequired
 };
 
 export default BurgerIngredientsCategory;
