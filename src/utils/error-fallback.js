cd5508a033151b1b980e02ecb48c08cb5ad19e52
danielginz <42783698+danielginz@users.noreply.github.com>
2023-03-17 16:34:09 +0200

    Delete src directory

--- src/utils/error-fallback.js
+++
@@ -1,7 +0,0 @@
-export const ErrorFallback = ({ error }) => (
-    <div>
-        <p>Something went wrong...</p>
-        {error.message && <span>Here's the error: {error.message}</span>}
-    </div>
-);
-export default ErrorFallback;
