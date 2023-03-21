c2b043de5235045577c983f53689071ddd3f64c4
danielginz <42783698+danielginz@users.noreply.github.com>
2023-03-21 17:32:04 +0200

    Add files via upload

---
+++ src/utils/prop-types.js
@@ -0,0 +1,49 @@
+import PropTypes from "prop-types";
+
+export const ingredientType = PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
+    __v: PropTypes.number.isRequired,
+    _id: PropTypes.string.isRequired,
+    calories: PropTypes.number.isRequired,
+    carbohydrates: PropTypes.number.isRequired,
+    fat: PropTypes.number.isRequired,
+    proteins: PropTypes.number.isRequired,
+    type: PropTypes.string.isRequired,
+    image_large: PropTypes.string.isRequired,
+    image_mobile: PropTypes.string.isRequired
+});
+
+export const ingredientSimplifiedType = PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
+    __v: PropTypes.number.isRequired,
+    _id: PropTypes.string.isRequired
+});
+
+export const ingredientSimplifiedType2 = PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    _id: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired
+});
+
+export const ingredientSimplifiedType3 = PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    __v: PropTypes.number.isRequired,
+    type: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired
+});
+
+export const ingredientSimplifiedType4 = PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    image_large: PropTypes.string.isRequired,
+    calories: PropTypes.number.isRequired,
+    proteins: PropTypes.number.isRequired,
+    fat: PropTypes.number.isRequired,
+    carbohydrates: PropTypes.number.isRequired
+});
