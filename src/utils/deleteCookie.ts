5cb00a699f193e22d0f1ec5380cdc3ddc5391ec0
danielginz <42783698+danielginz@users.noreply.github.com>
2023-05-15 10:26:32 +0300

    Delete src directory

--- src/utils/deleteCookie.ts
+++
@@ -1,5 +0,0 @@
-import { setCookie } from "./setCookie";
-
-export function deleteCookie(name: string) {
-    setCookie(name, null, { expires: -1 });
-}
--- src/utils/uslessMove.ts
+++
@@ -1,9 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, AppThunk } from '../index';
-import { storeType } from './types';
-import {AnyAction, Dispatch} from "redux";
-//export const useAppDispatch = () => useDispatch<AppThunk<AppDispatch>>()
-export const useAppDispatch = () => useDispatch<any>();
-
-//export const useAppDispatch = () => useDispatch<Dispatch<AnyAction>>();
-export const useAppSelector: TypedUseSelectorHook<storeType> = useSelector
