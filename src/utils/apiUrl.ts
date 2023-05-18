f1a260db386b2fc80569aac341568702f29e8240
danielginz <42783698+danielginz@users.noreply.github.com>
2023-05-18 17:09:54 +0300

    Delete src directory

--- src/utils/apiUrl.ts
+++
@@ -1,4 +0,0 @@
-export const baseUrl = 'https://norma.nomoreparties.space/api'
-
-export const wsUrl = 'wss://norma.nomoreparties.space/orders/all'
-export const authWsUrl= 'wss://norma.nomoreparties.space/orders'
--- src/utils/sum.ts
+++
@@ -1,8 +0,0 @@
-export default function sum(arr : string[]){
-    let answ = new Map()
-    arr.forEach(element => {
-      !answ.has(element) ? answ.set(element, 1) : answ.set(element, answ.get(element) + 1)
-    })
-   
-    return Array.from(answ)
-   }
