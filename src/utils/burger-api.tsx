a1eed7767732e63a8823485259777ff28433bf8b
danielginz <42783698+danielginz@users.noreply.github.com>
2023-05-20 22:37:09 +0300

    Delete src directory

--- src/utils/burger-api.tsx
+++
@@ -1,3 +0,0 @@
-export const NORMA_API:string = "https://norma.nomoreparties.space/api";
-export const ALL_ORDERS_WS_URL: string = 'wss://norma.nomoreparties.space/orders/all';
-export const USER_ORDERS_WS_URL: string = 'wss://norma.nomoreparties.space/orders';
--- src/utils/constants.tsx
+++
@@ -1,3 +0,0 @@
-export const BUN:string = "bun";
-export const SAUCE:string = "sauce";
-export const MAIN:string = "main";
