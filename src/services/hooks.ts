0c76264758061c4fcd808e4eb16c98390bd57713
danielginz <42783698+danielginz@users.noreply.github.com>
2023-05-20 22:45:55 +0300

    Add files via upload

--- src/services/hooks.ts
+++ src/services/hooks.ts
@@ -1,8 +1,5 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import type { RootState, AppDispatch } from './store'
-//import {storeType} from "./types";
 
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
-//export const useAppSelector: TypedUseSelectorHook<any> = useSelector
-//export const useAppSelector: TypedUseSelectorHook<storeType> = useSelector
